2a0f30395cc45870b361866d1f01277608e05a37
Daljit Kaur <daljit_kaur@softprodigy.com>
2024-07-02 19:06:28 +0530

    Meshcapade API created

---
+++ MeshcapadeDemo.Api/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using MeshcapadeDemo.Api.Services;
+using MeshcapadeDemo.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace MeshcapadeDemo.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly MeshcapadeService _meshcapadeService;
+
+        public HomeController(MeshcapadeService meshcapadeService)
+        {
+            _meshcapadeService = meshcapadeService;
+        }
+
+        [HttpGet("Heartbeat")]
+        public async Task<IActionResult> Heartbeat()
+        {
+            return Ok(new { message = "API endpoint is working fine!" });
+        }
+
+
+        [HttpGet("Login")]
+        public async Task<LoginResponse> Login(string username, string password)
+        {
+            try
+            {
+                var result = await _meshcapadeService.Login(username, password);
+                return result;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        [HttpPost("CreateAvatarFromImage")]
+        public async Task<bool> CreateAvatarFromImage(string token, IFormFile uploadedFile)
+        {
+            try
+            {
+                var assetID = await _meshcapadeService.InitiateAvatarCreation(token, uploadedFile);
+
+                // Call GetPath method to retrieve path using assetID
+                var path = await _meshcapadeService.RequestImageUploads(token, assetID);
+
+                var isUploaded = await _meshcapadeService.UploadImageToS3(token, uploadedFile, path);
+                if (isUploaded)
+                    return (await _meshcapadeService.StartFittingProcess(assetID, token));
+                return false;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+       
+    }
+}
---
+++ MeshcapadeDemo.Api/Services/MeshcapadeService.cs
@@ -0,0 +1,111 @@
+﻿using MeshcapadeDemo.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+namespace MeshcapadeDemo.Api.Services
+{
+    public class MeshcapadeService
+    {
+        private readonly HttpClient _httpClient;
+        public MeshcapadeService(HttpClient httpClient) { 
+            _httpClient = httpClient;
+        }
+
+        public async Task<LoginResponse> Login(string username, string password)
+        {
+            string url = "https://auth.meshcapade.com/realms/meshcapade-me/protocol/openid-connect/token";
+
+            var formContent = new FormUrlEncodedContent(new[]
+            {
+            new KeyValuePair<string, string>("grant_type", "password"),
+            new KeyValuePair<string, string>("client_id", "meshcapade-me"),
+            new KeyValuePair<string, string>("username", username),
+            new KeyValuePair<string, string>("password", password)
+            });
+
+            // Set the Content-Type header
+            formContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            HttpResponseMessage response = await _httpClient.PostAsync(url, formContent);
+
+            // Ensure the response status code is successful
+            response.EnsureSuccessStatusCode();
+
+            // Read and return the response content
+            var result = await response.Content.ReadFromJsonAsync<LoginResponse>();
+            return result;
+        }
+
+        public async Task<string> InitiateAvatarCreation(string token, IFormFile imgFile)
+        {
+            var url = "https://api.meshcapade.com/api/v1/avatars/create/from-images";
+            var formData = new MultipartFormDataContent();
+
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+            formData.Add(new StringContent(token), "token");
+
+            using (var stream = imgFile.OpenReadStream())
+            {
+                formData.Add(new StreamContent(stream), "file", imgFile.FileName);
+
+                var response = await _httpClient.PostAsync(url, formData);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new HttpRequestException($"Failed to create avatar: {response.ReasonPhrase}");
+                }
+                var responseData = await response.Content.ReadAsStringAsync();
+                var jsonObject = JObject.Parse(responseData);
+                string assetId = jsonObject["data"]["id"].ToString();
+                return assetId;
+            }
+        }
+
+        public async Task<string> RequestImageUploads(string token, string assetID)
+        {
+            var url = $"https://api.meshcapade.com/api/v1/avatars/{assetID}/images";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+
+            request.Headers.Add("Authorization", $"Bearer {token}");
+            var response = await _httpClient.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Failed to create avatar: {response.ReasonPhrase}");
+            }
+            var responseData = await response.Content.ReadAsStringAsync();
+            var jsonObject = JObject.Parse(responseData);
+            string path = jsonObject["data"]["attributes"]["url"]["path"].ToString();
+
+            return path;
+        }
+
+        public async Task<bool> UploadImageToS3(string token, IFormFile file, string uploadUrl)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Put, uploadUrl);
+            using (var content = new StreamContent(file.OpenReadStream()))
+            {
+                request.Content = content;
+                var response = await client.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public async Task<bool> StartFittingProcess(string assetId, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.PostAsync($"avatars/{assetId}/fit-to-images", null);
+            response.EnsureSuccessStatusCode();
+            return true;
+        }
+    }
+}
